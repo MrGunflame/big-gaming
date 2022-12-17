0fc6a8e88f86f3ad21643e213b7a8af964329abe
MrGunflame <mrgunflame@protonmail.com>
2022-12-17 11:46:07 +0100

    Add debug menu trigger

--- client/src/ui/events.rs
+++ client/src/ui/events.rs
@@ -6,15 +6,17 @@ use bevy::window::Windows;
 use crate::plugins::hotkeys::{Event, EventId, HotkeyStore, TriggerKind};
 
 use super::cursor::Cursor;
-use super::interfaces::{MENU_DEATH, MENU_GAME};
+use super::interfaces::{MENU_DEATH, MENU_DEBUG, MENU_GAME};
 use super::{menu, Focus, InterfaceState};
 
 const DEFAULT_TRIGGER_GAMEMENU: KeyCode = KeyCode::Escape;
+const DEFAULT_TRIGGER_DEBUGMENU: KeyCode = KeyCode::F3;
 
 static mut EVENTS: MaybeUninit<Events> = MaybeUninit::uninit();
 
 struct Events {
     game_menu: EventId,
+    debug_menu: EventId,
 }
 
 pub(super) fn register_events(mut hotkeys: ResMut<HotkeyStore>) {
@@ -24,8 +26,17 @@ pub(super) fn register_events(mut hotkeys: ResMut<HotkeyStore>) {
             .kind(TriggerKind::Trigger),
     );
 
+    let debug_menu = hotkeys.register(
+        Event::new()
+            .trigger(DEFAULT_TRIGGER_DEBUGMENU)
+            .kind(TriggerKind::Trigger),
+    );
+
     unsafe {
-        EVENTS.write(Events { game_menu });
+        EVENTS.write(Events {
+            game_menu,
+            debug_menu,
+        });
     }
 }
 
@@ -53,6 +64,16 @@ pub(super) fn handle_events(
 
             *focus = Focus::Interface;
             Cursor::unlock(window);
+        }
+    }
+
+    if hotkeys.triggered(events.debug_menu) {
+        if state.contains(MENU_DEBUG) {
+            unsafe {
+                state.remove::<_, ()>(MENU_DEBUG);
+            }
+        } else {
+            state.insert::<()>(MENU_DEBUG, None);
         }
     }
 }
