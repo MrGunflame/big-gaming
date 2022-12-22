79fde79baa2df4738f1cbb2b97a8145d83962f0a
MrGunflame <mrgunflame@protonmail.com>
2022-12-22 02:10:43 +0100

    feat: Add ServerEntity

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,7 +1,9 @@
 //! The common core of the game systems and components.
 //!
 //!
+#![feature(const_trait_impl)]
 #![feature(const_option)]
+#![feature(const_mut_refs)]
 
 pub mod archive;
 pub mod components;
@@ -9,6 +11,7 @@ pub mod ecs;
 pub mod id;
 pub mod localization;
 pub mod module;
+pub mod net;
 pub mod savefile;
 pub mod types;
 pub mod uuid;
