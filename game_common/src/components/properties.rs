3902fd27b2e6df108910fdb0e483410dcbaefc61
MrGunflame <mrgunflame@protonmail.com>
2023-04-14 05:20:12 +0200

    Add the None and Entity property types

--- game_common/src/components/properties.rs
+++ game_common/src/components/properties.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use crate::entity::EntityId;
+
 #[derive(Clone, Debug, Default)]
 pub struct Properties {
     props: Vec<Property>,
@@ -26,24 +28,33 @@ pub struct Property {
 
 #[derive(Clone, Debug)]
 pub enum PropertyValue {
+    /// A property that does not carry any value.
+    ///
+    /// `None` properties can be used as a marker property.
+    None,
     I32(i32),
     I64(i64),
     Bytes(Box<[u8]>),
+    Entity(EntityId),
 }
 
 impl PropertyValue {
     pub fn kind(&self) -> PropertyKind {
         match self {
+            Self::None => PropertyKind::None,
             Self::I32(_) => PropertyKind::I32,
             Self::I64(_) => PropertyKind::I64,
             Self::Bytes(_) => PropertyKind::Bytes,
+            Self::Entity(_) => PropertyKind::Entity,
         }
     }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum PropertyKind {
+    None,
     I32,
     I64,
     Bytes,
+    Entity,
 }
