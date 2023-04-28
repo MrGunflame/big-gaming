668f27a6981e3a07927ecec5722f5b498e15fa4c
MrGunflame <mrgunflame@protonmail.com>
2023-04-29 01:16:07 +0200

    Add docs to event macros

--- game_wasm/src/events.rs
+++ game_wasm/src/events.rs
@@ -1,4 +1,81 @@
+//! Events dispatched from the game, handled by a script
+//!
+
+/// An action event.
+///
+/// An action event is fired when the action that references this script is called.
+///
+/// This event takes a different signature depending on context that it is called in.
 pub use game_macros::wasm__event_on_action as on_action;
+
+/// A collision event.
+///
+/// A collision event is fired when the entity collides with another entity.
+///
+/// # Event signature
+///
+/// `fn(entity: `[`EntityId`]`, other: `[`EntityId`]`)`
+///
+/// # Examples
+///
+/// ```
+/// use game_wasm::entity::EntityId;
+/// use game_wasm::events::on_collision;
+///
+/// #[on_collision]
+/// fn on_collision(entity: EntityId, other: EntityId) {
+///     info!("{} collided with {}!", entity, other);
+/// }
+/// ```
+///
+/// [`EntityId`]: crate::entity::EntityId
+/// [`InventoryId`]: crate::inventory::InventoryId
 pub use game_macros::wasm__event_on_collision as on_collision;
+
+/// A item equip event.
+///
+/// A equip event is fired when the actor equippes an item.
+///
+/// # Event signature
+///
+/// `fn(item: `[`InventoryId`]`, actor: `[`EntityId`]`)`
+///
+/// # Examples
+///
+/// ```
+/// use game_wasm::entity::EntityId;
+/// use game_wasm::events::on_equip;
+///
+/// #[on_equip]
+/// fn on_equip(item: InventoryId, actor: EntityId) {
+///     info!("{} equipped {}!", actor, item);
+/// }
+/// ```
+///
+/// [`EntityId`]: crate::entity::EntityId
+/// [`InventoryId`]: crate::inventory::InventoryId
 pub use game_macros::wasm__event_on_equip as on_equip;
+
+/// A item unequip event.
+///
+/// A unequip event is fired when the actor unequippes an item.
+///
+/// # Event signature
+///
+/// `fn(item: `[`InventoryId`]`, actor: `[`EntityId`]`)`
+///
+/// # Examples
+///
+/// ```
+/// use game_wasm::entity::EntityId;
+/// use game_wasm::events::on_unequip;
+///
+/// #[on_unequip]
+/// fn on_unequip(item: InventoryId, actor: EntityId) {
+///     info!("{} unequipped {}!", actor, item);
+/// }
+/// ```
+///
+/// [`EntityId`]: crate::entity::EntityId
+/// [`InventoryId`]: crate::inventory::InventoryId
 pub use game_macros::wasm__event_on_unequip as on_unequip;
