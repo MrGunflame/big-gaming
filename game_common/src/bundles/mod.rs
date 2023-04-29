80b22ffc857be2465a594e749318c51eb5881f25
MrGunflame <mrgunflame@protonmail.com>
2023-04-29 02:04:45 +0200

    Remove rendering deps, take 2

--- game_common/src/bundles/mod.rs
+++ game_common/src/bundles/mod.rs
@@ -4,12 +4,10 @@ mod actor;
 mod combat;
 mod object;
 mod player;
-mod projectile;
 mod transform;
 
 pub use actor::ActorBundle;
 pub use combat::CombatBundle;
 pub use object::ObjectBundle;
 pub use player::HostPlayerBundle;
-pub use projectile::ProjectileBundle;
 pub use transform::TransformBundle;
