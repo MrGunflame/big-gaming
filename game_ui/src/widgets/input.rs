0aa2f01b337274949b30281c0b43d67160e6dc5a
MrGunflame <mrgunflame@protonmail.com>
2023-05-24 17:45:10 +0200

    Handle deletion of UTF8 sequences correctly

--- game_ui/src/widgets/input.rs
+++ game_ui/src/widgets/input.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use crate::events::EventHandlers;
 use crate::reactive::{create_signal, Node, Scope};
 use crate::render::style::Style;
@@ -16,7 +18,7 @@ impl Component for Input {
     type Properties = InputProps;
 
     fn render(cx: &Scope, props: Self::Properties) -> Scope {
-        let (value, set_value) = create_signal(cx, props.value);
+        let (value, set_value) = create_signal(cx, Buffer::new(props.value));
 
         let root = cx.push(Node {
             element: Element {
@@ -33,12 +35,12 @@ impl Component for Input {
                     }
                     // Backspace
                     '\u{8}' => set_value.update(|string| {
-                        if !string.is_empty() {
-                            string.remove(string.len() - 1);
-                        }
+                        string.remove_prev();
                     }),
                     // Delete
-                    '\u{7F}' => {}
+                    '\u{7F}' => set_value.update(|string| {
+                        string.remove_next();
+                    }),
                     _ => {
                         if !char.is_control() {
                             set_value.update(|string| string.push(char));
@@ -52,10 +54,122 @@ impl Component for Input {
         Text::render(
             &root,
             TextProps {
-                text: (move || value.get()).into(),
+                text: (move || value.get().to_string()).into(),
             },
         );
 
         root
+    }
+}
+
+/// A UTF-8 string buffer.
+#[derive(Clone, Debug)]
+struct Buffer {
+    string: String,
+    /// Position of the cursor.
+    cursor: usize,
+}
+
+impl Buffer {
+    fn new(string: String) -> Self {
+        let cursor = string
+            .char_indices()
+            .last()
+            .map(|(i, c)| i + c.len_utf8())
+            .unwrap_or(0);
+
+        Self { string, cursor }
+    }
+
+    fn push(&mut self, ch: char) {
+        self.string.push(ch);
+        self.cursor += ch.len_utf8();
+    }
+
+    fn remove_next(&mut self) {
+        if let Some(s) = self.string.get(self.cursor..) {
+            if s.is_empty() {
+                return;
+            }
+
+            // self.cursor exists means that at least 1 char exists.
+            let ch = s.chars().nth(0).unwrap();
+
+            self.string.remove(self.cursor);
+        }
+    }
+
+    fn remove_prev(&mut self) {
+        let s = &self.string[..self.cursor];
+
+        if let Some(ch) = s.chars().last() {
+            self.string.remove(self.cursor - ch.len_utf8());
+            self.cursor -= ch.len_utf8();
+        }
+    }
+}
+
+impl Deref for Buffer {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.string
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Buffer;
+
+    #[test]
+    fn buffer_new_empty() {
+        let string = String::new();
+
+        let buffer = Buffer::new(string.clone());
+
+        assert_eq!(buffer.string, string);
+        assert_eq!(buffer.cursor, 0);
+    }
+
+    #[test]
+    fn buffer_from_ascii() {
+        let string = String::from("test");
+
+        let buffer = Buffer::new(string.clone());
+
+        assert_eq!(buffer.string, string);
+        assert_eq!(buffer.cursor, 4);
+    }
+
+    #[test]
+    fn buffer_from_unicode() {
+        let string = String::from("testö");
+
+        let buffer = Buffer::new(string.clone());
+
+        assert_eq!(buffer.string, string);
+        assert_eq!(buffer.cursor, 6);
+    }
+
+    #[test]
+    fn buffer_remove_next() {
+        let string = String::from("testö");
+
+        let mut buffer = Buffer::new(string.clone());
+        buffer.remove_next();
+
+        assert_eq!(buffer.string, string);
+        assert_eq!(buffer.cursor, 6);
+    }
+
+    #[test]
+    fn buffer_remove_prev() {
+        let string = String::from("testö");
+
+        let mut buffer = Buffer::new(string.clone());
+        buffer.remove_prev();
+
+        assert_eq!(buffer.string, "test");
+        assert_eq!(buffer.cursor, 4);
     }
 }
