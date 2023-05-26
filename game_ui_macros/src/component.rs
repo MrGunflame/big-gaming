c159086fc8a9b83251c6e4e0591bd25c1e790bbd
MrGunflame <mrgunflame@protonmail.com>
2023-05-26 13:23:26 +0200

    Support lifetimes/references for components

--- game_ui_macros/src/component.rs
+++ game_ui_macros/src/component.rs
@@ -1,10 +1,8 @@
-use std::collections::HashMap;
-
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use quote::quote;
 use syn::parse::Parse;
-use syn::{parse_macro_input, FnArg, ItemFn, Pat};
+use syn::{parse_macro_input, FnArg, ItemFn, Lifetime, Pat, Type, Visibility};
 
 pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
     parse_macro_input!(attr as Empty);
@@ -27,31 +25,35 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
         }
     }
 
+    if item.sig.asyncness.is_some() {
+        panic!("component functions cannot be async");
+    }
+
+    if item.sig.unsafety.is_some() {
+        panic!("component functions cannot be unsafe");
+    }
+
+    if item.sig.constness.is_some() {
+        panic!("component functions cannot be const");
+    }
+
+    let props = Properties::new(props);
+
     let vis = item.vis.clone();
     let ident = item.sig.ident.clone();
-    let component_struct = quote! {
-        #vis struct #ident;
-    };
-
-    let fields: TokenStream2 = props
-        .iter()
-        .map(|(id, ty)| {
-            quote! {
-                #vis #id: #ty,
-            }
-        })
-        .collect();
+    let component_struct = expand_component_struct(&vis, ident.clone(), &props);
 
     let props_ident = Ident::new(&format!("{}Props", ident), Span::call_site());
-    let props_struct = quote! {
-        #vis struct #props_ident {
-            #fields
-        }
-    };
+    let props_struct = expand_property_struct(&vis, props_ident.clone(), &props);
+
+    let props_sig = expand_property_struct_sig(props_ident, &props);
 
     let call_args: TokenStream2 = props
+        .props
         .iter()
-        .map(|(id, _)| {
+        .map(|p| {
+            let id = p.ident.clone();
+
             quote! {
                 props.#id,
             }
@@ -65,9 +67,11 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
     item.sig.ident = Ident::new("render_component", Span::call_site());
     let fn_ident = item.sig.ident.clone();
 
+    let lifetimes = props.struct_lifetimes();
+
     let component_impl = quote! {
-        impl ::game_ui::widgets::Component for #ident {
-            type Properties = #props_ident;
+        impl<#lifetimes> ::game_ui::widgets::Component for #ident<#lifetimes> {
+            type Properties = #props_sig;
 
             fn render(cx: &::game_ui::reactive::Scope, props: Self::Properties) -> ::game_ui::reactive::Scope {
                 // Always inline as this is the single call site.
@@ -97,5 +101,133 @@ impl Parse for Empty {
         } else {
             Err(input.error("macro does not take any attributes"))
         }
+    }
+}
+
+struct Properties {
+    props: Vec<Property>,
+}
+
+struct Property {
+    ident: Ident,
+    lifetime: Option<Lifetime>,
+    ty: Type,
+}
+
+impl Property {
+    fn expand_struct_field(&self) -> TokenStream2 {
+        let ident = self.ident.clone();
+        let lifetime = self.lifetime.clone();
+        let ty = self.ty.clone();
+
+        if let Some(lifetime) = lifetime {
+            quote! {
+                pub #ident: &#lifetime #ty,
+            }
+        } else {
+            quote! {
+                pub #ident: #ty,
+            }
+        }
+    }
+}
+
+impl Properties {
+    fn new(input: Vec<(Ident, Box<Type>)>) -> Self {
+        let mut props = Vec::new();
+        for (index, (ident, ty)) in input.into_iter().enumerate() {
+            let prop = match *ty {
+                Type::Reference(rf) => {
+                    let lifetime = match rf.lifetime {
+                        Some(lt) => lt,
+                        None => {
+                            let id = format!("'l{}", index);
+                            Lifetime::new(&id, Span::call_site())
+                        }
+                    };
+
+                    Property {
+                        ident,
+                        lifetime: Some(lifetime),
+                        ty: *rf.elem,
+                    }
+                }
+                ty => Property {
+                    ident,
+                    lifetime: None,
+                    ty,
+                },
+            };
+
+            props.push(prop);
+        }
+
+        Self { props }
+    }
+
+    fn struct_lifetimes(&self) -> TokenStream2 {
+        self.props
+            .iter()
+            .map(|prop| {
+                let lifetime = prop.lifetime.clone();
+
+                if let Some(lifetime) = lifetime {
+                    quote! { #lifetime, }
+                } else {
+                    quote! {}
+                }
+            })
+            .collect()
+    }
+}
+
+fn expand_component_struct(vis: &Visibility, ident: Ident, props: &Properties) -> TokenStream2 {
+    let lifetimes = props.struct_lifetimes();
+
+    let fields: TokenStream2 = props
+        .props
+        .iter()
+        .enumerate()
+        .map(|(index, p)| {
+            if let Some(lifetime) = p.lifetime.clone() {
+                let ident = Ident::new(&format!("_l{}", index), Span::call_site());
+
+                quote! {
+                    #ident: ::core::marker::PhantomData<fn() -> &#lifetime ()>,
+                }
+            } else {
+                quote! {}
+            }
+        })
+        .collect();
+
+    quote! {
+        #vis struct #ident<#lifetimes> {
+            #fields
+        }
+    }
+}
+
+fn expand_property_struct(vis: &Visibility, ident: Ident, props: &Properties) -> TokenStream2 {
+    let sig = expand_property_struct_sig(ident, props);
+
+    let fields: TokenStream2 = props
+        .props
+        .iter()
+        .map(|prop| prop.expand_struct_field())
+        .collect();
+
+    quote! {
+        #vis struct #sig {
+            #fields
+        }
+    }
+}
+
+fn expand_property_struct_sig(ident: Ident, props: &Properties) -> TokenStream2 {
+    let lifetimes = props.struct_lifetimes();
+
+    quote! {
+        #ident<#lifetimes>
     }
 }
