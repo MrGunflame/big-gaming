a3340921497f715689fa34469f3577c62e50b07f
MrGunflame <mrgunflame@protonmail.com>
2023-05-30 16:05:01 +0200

    Add custom `TimePlugin`

---
+++ game_core/src/time.rs
@@ -0,0 +1,54 @@
+//! Time plugin for advancing frame times
+//!
+//! Not to be confused with in-game time.
+
+use std::time::{Duration, Instant};
+
+use bevy_app::{App, Plugin};
+use bevy_ecs::system::{ResMut, Resource};
+
+#[derive(Copy, Clone, Debug, Default)]
+pub struct TimePlugin;
+
+impl Plugin for TimePlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(Time::new());
+        app.add_system(advance_time);
+    }
+}
+
+#[derive(Clone, Debug, Resource)]
+pub struct Time {
+    startup: Instant,
+    speed: f64,
+    delta: Duration,
+    last_update: Instant,
+}
+
+impl Time {
+    pub fn new() -> Self {
+        let now = Instant::now();
+
+        Self {
+            startup: now,
+            speed: 1.0,
+            delta: Duration::ZERO,
+            last_update: now,
+        }
+    }
+
+    pub fn delta(&self) -> Duration {
+        self.delta
+    }
+
+    fn update(&mut self) {
+        let now = Instant::now();
+
+        self.delta = now - self.last_update;
+        self.last_update = now;
+    }
+}
+
+fn advance_time(mut time: ResMut<Time>) {
+    time.update();
+}
