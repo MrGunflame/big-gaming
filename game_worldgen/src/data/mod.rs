1a4202ebdbbea870a3e84cdecf2ffb7de14b20fd
MrGunflame <mrgunflame@protonmail.com>
2023-07-07 01:01:21 +0200

    Add world storage format

---
+++ game_worldgen/src/data/mod.rs
@@ -0,0 +1,211 @@
+//! The world data format.
+
+use bytes::{Buf, BufMut};
+use game_common::components::components::{Component, Components};
+use game_common::components::transform::Transform;
+use game_common::module::ModuleId;
+use game_common::record::{RecordId, RecordReference};
+use game_common::world::entity::EntityKind;
+use game_common::world::CellId;
+use glam::{Quat, Vec3};
+
+#[derive(Clone, Debug)]
+pub struct Cells {
+    id: CellId,
+    entities: Vec<Entity>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Entity {
+    id: RecordReference,
+    kind: EntityKind,
+    transform: Transform,
+    components: Components,
+}
+
+impl Encode for Entity {
+    fn encode<B>(&self, mut buf: B)
+    where
+        B: BufMut,
+    {
+        self.id.encode(&mut buf);
+        self.kind.encode(&mut buf);
+
+        self.transform.translation.x.encode(&mut buf);
+        self.transform.translation.y.encode(&mut buf);
+        self.transform.translation.z.encode(&mut buf);
+        self.transform.rotation.x.encode(&mut buf);
+        self.transform.rotation.y.encode(&mut buf);
+        self.transform.rotation.z.encode(&mut buf);
+        self.transform.rotation.w.encode(&mut buf);
+        self.transform.scale.x.encode(&mut buf);
+        self.transform.scale.y.encode(&mut buf);
+        self.transform.scale.z.encode(&mut buf);
+
+        (self.components.len() as u64).encode(&mut buf);
+        for (id, comp) in self.components.iter() {
+            id.encode(&mut buf);
+
+            (comp.len() as u64).encode(&mut buf);
+            buf.put_slice(&comp.bytes);
+        }
+    }
+}
+
+impl Decode for Entity {
+    type Error = ();
+
+    fn decode<B>(mut buf: B) -> Result<Self, Self::Error>
+    where
+        B: Buf,
+    {
+        let id = RecordReference::decode(&mut buf).unwrap();
+        let kind = EntityKind::decode(&mut buf).unwrap();
+
+        let tx = f32::decode(&mut buf).unwrap();
+        let ty = f32::decode(&mut buf).unwrap();
+        let tz = f32::decode(&mut buf).unwrap();
+        let rx = f32::decode(&mut buf).unwrap();
+        let ry = f32::decode(&mut buf).unwrap();
+        let rz = f32::decode(&mut buf).unwrap();
+        let rw = f32::decode(&mut buf).unwrap();
+        let sx = f32::decode(&mut buf).unwrap();
+        let sy = f32::decode(&mut buf).unwrap();
+        let sz = f32::decode(&mut buf).unwrap();
+
+        let transform = Transform {
+            translation: Vec3::new(tx, ty, tz),
+            rotation: Quat::from_xyzw(rx, ry, rz, rw),
+            scale: Vec3::new(sx, sy, sz),
+        };
+
+        let len = u64::decode(&mut buf).unwrap();
+        let mut components = Components::new();
+        for _ in 0..len {
+            let id = RecordReference::decode(&mut buf).unwrap();
+
+            let len = u64::decode(&mut buf).unwrap();
+            let mut bytes = vec![];
+            for _ in 0..len {
+                bytes.push(u8::decode(&mut buf).unwrap());
+            }
+
+            components.insert(id, Component { bytes });
+        }
+
+        Ok(Self {
+            id,
+            kind,
+            transform,
+            components,
+        })
+    }
+}
+
+pub trait Encode {
+    fn encode<B>(&self, buf: B)
+    where
+        B: BufMut;
+}
+
+pub trait Decode: Sized {
+    type Error;
+
+    fn decode<B>(buf: B) -> Result<Self, Self::Error>
+    where
+        B: Buf;
+}
+
+impl Encode for RecordReference {
+    fn encode<B>(&self, mut buf: B)
+    where
+        B: BufMut,
+    {
+        buf.put_slice(&self.module.into_bytes());
+        buf.put_slice(&self.record.0.to_le_bytes());
+    }
+}
+
+impl Decode for RecordReference {
+    type Error = ();
+
+    fn decode<B>(mut buf: B) -> Result<Self, Self::Error>
+    where
+        B: Buf,
+    {
+        let mut module = [0; 16];
+        buf.copy_to_slice(&mut module);
+        let mut record = [0; 4];
+        buf.copy_to_slice(&mut record);
+
+        Ok(Self {
+            module: ModuleId::from_bytes(module),
+            record: RecordId(u32::from_le_bytes(record)),
+        })
+    }
+}
+
+impl Encode for EntityKind {
+    fn encode<B>(&self, buf: B)
+    where
+        B: BufMut,
+    {
+        let b: u8 = match self {
+            Self::Actor => 0,
+            Self::Item => 1,
+            Self::Object => 2,
+            Self::Terrain => 3,
+        };
+
+        b.encode(buf)
+    }
+}
+
+impl Decode for EntityKind {
+    type Error = ();
+
+    fn decode<B>(buf: B) -> Result<Self, Self::Error>
+    where
+        B: Buf,
+    {
+        let b = u8::decode(buf).unwrap();
+
+        match b {
+            0 => Ok(Self::Actor),
+            1 => Ok(Self::Item),
+            2 => Ok(Self::Object),
+            3 => Ok(Self::Terrain),
+            _ => todo!(),
+        }
+    }
+}
+
+macro_rules! impl_primitive {
+    ($($t:ty),*) => {
+        $(
+            impl Encode for $t {
+                fn encode<B>(&self, mut buf: B)
+                where
+                    B: BufMut,
+                {
+                    buf.put_slice(&self.to_le_bytes());
+                }
+            }
+
+            impl Decode for $t {
+                type Error = ();
+
+                fn decode<B>(mut buf: B) -> Result<Self, Self::Error>
+                where
+                    B: Buf,
+                {
+                    let mut bytes = [0; std::mem::size_of::<Self>()];
+                    buf.copy_to_slice(&mut bytes);
+                    Ok(Self::from_le_bytes(bytes))
+                }
+            }
+        )*
+    };
+}
+
+impl_primitive! { u8, u16, u32, u64, i8, i16, i32, i64, f32, f64 }
