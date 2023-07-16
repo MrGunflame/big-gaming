62f64464411d77659dedc767717ba4a840e8d6cc
MrGunflame <mrgunflame@protonmail.com>
2023-07-16 13:53:38 +0200

    Add `Quad` shape

--- game_render/src/shape/mod.rs
+++ game_render/src/shape/mod.rs
@@ -1,3 +1,5 @@
+use glam::Vec2;
+
 use crate::mesh::{Indices, Mesh};
 
 #[derive(Copy, Clone, Debug)]
@@ -162,6 +164,164 @@ impl From<Plane> for Mesh {
         mesh.set_indices(Indices::U32(indicies));
         mesh.set_normals(normals.to_vec());
         mesh.set_uvs(uvs.to_vec());
+        mesh.compute_tangents();
+        mesh
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Face {
+    Front,
+    Back,
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+/// A quad placed on a face.
+#[derive(Copy, Clone, Debug)]
+pub struct Quad {
+    pub size: Vec2,
+    pub face: Face,
+}
+
+impl From<Quad> for Mesh {
+    fn from(s: Quad) -> Self {
+        let positions;
+        let normals;
+        let uvs;
+
+        match s.face {
+            Face::Front => {
+                let min_x = -s.size.x / 2.0;
+                let max_x = s.size.x / 2.0;
+                let min_y = -s.size.y / 2.0;
+                let max_y = s.size.y / 2.0;
+
+                positions = vec![
+                    [min_x, min_y, 0.0],
+                    [max_x, min_y, 0.0],
+                    [max_x, max_y, 0.0],
+                    [min_x, max_y, 0.0],
+                ];
+                normals = vec![
+                    [0.0, 0.0, 1.0],
+                    [0.0, 0.0, 1.0],
+                    [0.0, 0.0, 1.0],
+                    [0.0, 0.0, 1.0],
+                ];
+                uvs = vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]];
+            }
+            Face::Back => {
+                let min_x = -s.size.x / 2.0;
+                let max_x = s.size.x / 2.0;
+                let min_y = -s.size.y / 2.0;
+                let max_y = s.size.y / 2.0;
+
+                positions = vec![
+                    [min_x, max_y, 0.0],
+                    [max_x, max_y, 0.0],
+                    [max_x, max_y, 0.0],
+                    [min_x, min_y, 0.0],
+                ];
+                normals = vec![
+                    [0.0, 0.0, -1.0],
+                    [0.0, 0.0, -1.0],
+                    [0.0, 0.0, -1.0],
+                    [0.0, 0.0, -1.0],
+                ];
+                uvs = vec![[1.0, 0.0], [0.0, 0.0], [0.0, 1.0], [1.0, 1.0]];
+            }
+            Face::Right => {
+                let min_y = -s.size.y / 2.0;
+                let max_y = s.size.y / 2.0;
+                let min_z = -s.size.x / 2.0;
+                let max_z = s.size.x / 2.0;
+
+                positions = vec![
+                    [0.0, min_y, min_z],
+                    [0.0, max_y, min_z],
+                    [0.0, max_y, max_z],
+                    [0.0, min_y, max_z],
+                ];
+                normals = vec![
+                    [1.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                ];
+                uvs = vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]];
+            }
+            Face::Left => {
+                let min_y = -s.size.y / 2.0;
+                let max_y = s.size.y / 2.0;
+                let min_z = -s.size.x / 2.0;
+                let max_z = s.size.x / 2.0;
+
+                positions = vec![
+                    [0.0, min_y, max_z],
+                    [0.0, max_y, max_z],
+                    [0.0, max_y, min_z],
+                    [0.0, min_y, min_z],
+                ];
+                normals = vec![
+                    [-1.0, 0.0, 0.0],
+                    [-1.0, 0.0, 0.0],
+                    [-1.0, 0.0, 0.0],
+                    [-1.0, 0.0, 0.0],
+                ];
+                uvs = vec![[1.0, 0.0], [0.0, 0.0], [0.0, 1.0], [1.0, 1.0]];
+            }
+            Face::Top => {
+                let min_x = -s.size.x / 2.0;
+                let max_x = s.size.x / 2.0;
+                let min_z = -s.size.y / 2.0;
+                let max_z = s.size.y / 2.0;
+
+                positions = vec![
+                    [max_x, 0.0, min_z],
+                    [min_x, 0.0, min_z],
+                    [min_x, 0.0, max_z],
+                    [max_x, 0.0, max_z],
+                ];
+                normals = vec![
+                    [0.0, 1.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                ];
+                uvs = vec![[1.0, 0.0], [0.0, 0.0], [0.0, 1.0], [1.0, 1.0]];
+            }
+            Face::Bottom => {
+                let min_x = -s.size.x / 2.0;
+                let max_x = s.size.x / 2.0;
+                let min_z = -s.size.y / 2.0;
+                let max_z = s.size.y / 2.0;
+
+                positions = vec![
+                    [max_x, 0.0, max_z],
+                    [min_x, 0.0, max_z],
+                    [min_x, 0.0, min_z],
+                    [max_x, 0.0, min_z],
+                ];
+                normals = vec![
+                    [0.0, -1.0, 0.0],
+                    [0.0, -1.0, 0.0],
+                    [0.0, -1.0, 0.0],
+                    [0.0, -1.0, 0.0],
+                ];
+                uvs = vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]];
+            }
+        }
+
+        let indices = Indices::U16(vec![0, 1, 2, 2, 3, 0]);
+
+        let mut mesh = Mesh::new();
+        mesh.set_positions(positions);
+        mesh.set_normals(normals);
+        mesh.set_indices(indices);
+        mesh.set_uvs(uvs);
         mesh.compute_tangents();
         mesh
     }
