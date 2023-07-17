2f415b7194e3b46e3e7d521c5925b5d90e38024a
MrGunflame <mrgunflame@protonmail.com>
2023-07-17 15:43:19 +0200

    Add `Ray` to common

--- game_common/src/math/mod.rs
+++ game_common/src/math/mod.rs
@@ -1,3 +1,22 @@
 mod rotation;
 
+use glam::Vec3;
 pub use rotation::{Rotation, RotationExt};
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn plane_intersection(&self, plane_origin: Vec3, plane_normal: Vec3) -> Option<Vec3> {
+        let denom = plane_normal.dot(self.direction);
+        if denom.abs() > f32::EPSILON {
+            let distance = (plane_origin - self.origin).dot(plane_normal) / denom;
+            Some(self.origin + self.direction * distance)
+        } else {
+            None
+        }
+    }
+}
