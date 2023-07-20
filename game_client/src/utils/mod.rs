5c1e3ee24c2f70ed4ba496d72065687d1cb5c8a1
MrGunflame <mrgunflame@protonmail.com>
2023-07-20 15:28:51 +0200

    Fix `extract_actor_rotation` returning `NaN`s

--- game_client/src/utils/mod.rs
+++ game_client/src/utils/mod.rs
@@ -17,10 +17,25 @@ pub fn extract_actor_rotation(rotation: Quat) -> Quat {
 
     let b = Vec3::Z;
 
-    let mut angle = (pt.dot(b)).acos();
+    let mut angle = f32::clamp(pt.dot(b), -1.0, 1.0).acos();
     if pt.x.is_sign_negative() {
         angle = -angle;
     }
 
-    Quat::from_axis_angle(Vec3::Y, angle + PI)
+    let res = Quat::from_axis_angle(Vec3::Y, angle + PI);
+    debug_assert!(!res.is_nan());
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use glam::Quat;
+
+    use crate::utils::extract_actor_rotation;
+
+    #[test]
+    fn extract_actor_rotation_nan() {
+        let input = Quat::from_xyzw(3.53899e-6, -1.0000173, -0.0050006974, -0.0007022657);
+        assert!(!extract_actor_rotation(input).is_nan());
+    }
 }
