f571fff9b53b42586b382f12ee0b93c25131d6e3
MrGunflame <mrgunflame@protonmail.com>
2023-07-21 14:06:15 +0200

    Implement action conversion

--- tools/json2dat/src/convert.rs
+++ tools/json2dat/src/convert.rs
@@ -1,4 +1,9 @@
+use std::path::PathBuf;
+
 use game_common::module::{Dependency, Module, Version};
+use game_data::components::actions::ActionRecord;
+use game_data::record::{Record, RecordBody};
+use game_data::uri::Uri;
 use game_data::{DataBuffer, Encode};
 
 use crate::types::Root;
@@ -18,6 +23,25 @@ pub fn encode(root: Root) -> Vec<u8> {
             })
             .collect(),
     });
+
+    for action in root.records.actions {
+        let record = Record {
+            id: action.id.0,
+            name: action.name,
+            scripts: action
+                .scripts
+                .into_iter()
+                .map(|s| Uri::from(PathBuf::from(s)))
+                .collect(),
+            body: RecordBody::Action(ActionRecord {
+                description: action.description,
+            }),
+        };
+
+        buffer.records.push(record);
+    }
+
+    buffer.header.records = buffer.records.len() as u32;
 
     let mut buf = Vec::new();
     buffer.encode(&mut buf);
--- tools/json2dat/src/types.rs
+++ tools/json2dat/src/types.rs
@@ -1,6 +1,7 @@
 use std::fmt::{self, Formatter};
 
 use game_common::module::ModuleId;
+use game_common::record::RecordId;
 use serde::de::{self, Visitor};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
@@ -9,12 +10,17 @@ pub struct Root {
     pub id: JsonModuleId,
     pub name: String,
     pub version: String,
+    #[serde(default)]
     pub dependencies: Vec<JsonDependency>,
-    pub records: Vec<Record>,
+    #[serde(default)]
+    pub records: Records,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Record {}
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+pub struct Records {
+    #[serde(default)]
+    pub actions: Vec<Action>,
+}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct JsonModuleId(pub ModuleId);
@@ -57,9 +63,58 @@ impl<'de> Deserialize<'de> for JsonModuleId {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct JsonRecordId(pub RecordId);
+
+impl Serialize for JsonRecordId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.0.to_string())
+    }
+}
+
+impl<'de> Deserialize<'de> for JsonRecordId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Vis;
+
+        impl<'de> Visitor<'de> for Vis {
+            type Value = JsonRecordId;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a hex-encoded record id")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                match v.parse() {
+                    Ok(val) => Ok(JsonRecordId(val)),
+                    Err(err) => Err(E::custom(err)),
+                }
+            }
+        }
+
+        deserializer.deserialize_str(Vis)
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct JsonDependency {
     pub id: JsonModuleId,
     pub name: Option<String>,
     pub version: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Action {
+    pub id: JsonRecordId,
+    pub name: String,
+    pub description: String,
+    pub scripts: Vec<String>,
 }
