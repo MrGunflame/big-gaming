71b854a22c2483a3c995d5221a1f1a19463bc533
MrGunflame <mrgunflame@protonmail.com>
2023-08-14 12:48:21 +0200

    Remove `bevy_ecs` and `bevy_app` deps from `core` crate

--- game_core/src/lib.rs
+++ game_core/src/lib.rs
@@ -3,38 +3,7 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 #![deny(unused_crate_dependencies)]
 
-use combat::CombatPlugin;
-use game_asset::AssetPlugin;
-use hierarchy::HierarchyPlugin;
-use modules::ModulePlugin;
-use transform::TransformPlugin;
-use world::{LevelPlugin, ObjectPlugin, SpawnPlugin, WorldTimePlugin};
-
-use bevy_app::{App, Plugin};
-
-pub mod combat;
 pub mod counter;
-pub mod hierarchy;
+pub mod logger;
 pub mod modules;
 pub mod time;
-pub mod transform;
-pub mod world;
-
-pub mod logger;
-
-#[derive(Copy, Clone, Debug, Default)]
-pub struct CorePlugins;
-
-impl Plugin for CorePlugins {
-    fn build(&self, app: &mut App) {
-        app.add_plugin(time::TimePlugin);
-
-        app.add_plugin(AssetPlugin::default());
-        app.add_plugin(CombatPlugin);
-        app.add_plugin(LevelPlugin);
-        app.add_plugin(ObjectPlugin);
-        app.add_plugin(WorldTimePlugin::default());
-        app.add_plugin(SpawnPlugin);
-        app.add_plugin(ModulePlugin);
-    }
-}
