0f16d13492ba87aa5d91d836c44701df72a0f48e
MrGunflame <mrgunflame@protonmail.com>
2023-09-10 04:25:14 +0200

    Reimplement `SpaceAround`

--- game_ui/src/render/layout.rs
+++ game_ui/src/render/layout.rs
@@ -486,7 +486,47 @@ impl LayoutTree {
                         }
                     }
                 }
-                _ => todo!(),
+                Justify::SpaceAround => {
+                    let num_children = child_bounds.len() as u32;
+
+                    let mut offset = 0;
+                    for (_, bounds) in &child_bounds {
+                        match elem.style.direction {
+                            Direction::Row => offset += bounds.x,
+                            Direction::Column => offset += bounds.y,
+                        }
+                    }
+
+                    let pad_zone = match elem.style.direction {
+                        Direction::Row => (content.height() - offset) / (num_children + 1),
+                        Direction::Column => (content.width() - offset) / (num_children + 1),
+                    };
+
+                    let mut next_position = content.min;
+                    match elem.style.direction {
+                        Direction::Row => next_position.y += pad_zone,
+                        Direction::Column => next_position.x += pad_zone,
+                    };
+
+                    for (key, bounds) in child_bounds {
+                        let layout = self.layouts.get_mut(key).unwrap();
+
+                        layout.position = next_position;
+                        layout.width = bounds.x;
+                        layout.height = bounds.y;
+
+                        match elem.style.direction {
+                            Direction::Row => {
+                                next_position.y += layout.height;
+                                next_position.y += pad_zone;
+                            }
+                            Direction::Column => {
+                                next_position.x += layout.width;
+                                next_position.x += pad_zone;
+                            }
+                        }
+                    }
+                }
             }
 
             let mut abs_child_bounds = vec![];
