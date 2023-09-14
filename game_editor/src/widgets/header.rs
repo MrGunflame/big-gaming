d8889cea82da3f1f660dc84760bbb754b290f638
MrGunflame <mrgunflame@protonmail.com>
2023-09-14 23:25:10 +0200

    Small padding around Header entries

--- game_editor/src/widgets/header.rs
+++ game_editor/src/widgets/header.rs
@@ -1,7 +1,7 @@
 use game_input::mouse::MouseButtonInput;
 use game_ui::events::Context;
 use game_ui::reactive::Scope;
-use game_ui::style::{Direction, Style};
+use game_ui::style::{Direction, Padding, Size, Style};
 use game_ui::widgets::{Button, Callback, Container, Text, Widget};
 
 pub struct Header {
@@ -12,11 +12,15 @@ impl Widget for Header {
     fn build(self, ctx: &Scope) -> Scope {
         let root = ctx.append(Container::new().style(Style {
             direction: Direction::Column,
+            padding: Padding::splat(Size::Pixels(3)),
             ..Default::default()
         }));
 
         for button in self.buttons {
-            let but = root.append(Button::new().on_click(button.on_click));
+            let but = root.append(Button::new().on_click(button.on_click).style(Style {
+                padding: Padding::splat(Size::Pixels(3)),
+                ..Default::default()
+            }));
             but.append(Text::new().text(button.label));
         }
 
