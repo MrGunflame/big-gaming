da0157b56c2f78c2a9b1703035e73fb0e55c7778
MrGunflame <mrgunflame@protonmail.com>
2023-10-14 02:09:52 +0200

    Add model to `RaceRecord`

--- game_data/src/components/race.rs
+++ game_data/src/components/race.rs
@@ -2,16 +2,20 @@ use bytes::{Buf, BufMut};
 use game_common::record::RecordReference;
 use thiserror::Error;
 
+use crate::uri::Uri;
 use crate::{Decode, Encode};
 
 #[derive(Clone, Debug, PartialEq, Eq, Error)]
 pub enum RaceRecordError {
+    #[error("failed to decode race model: {0}")]
+    Model(<Uri as Decode>::Error),
     #[error("failed to decode race actions: {0}")]
     Actions(<Vec<RecordReference> as Decode>::Error),
 }
 
 #[derive(Clone, Debug)]
 pub struct RaceRecord {
+    pub model: Uri,
     pub actions: Vec<RecordReference>,
 }
 
@@ -20,6 +24,7 @@ impl Encode for RaceRecord {
     where
         B: BufMut,
     {
+        self.model.encode(&mut buf);
         self.actions.encode(&mut buf);
     }
 }
@@ -31,8 +36,9 @@ impl Decode for RaceRecord {
     where
         B: Buf,
     {
+        let model = Uri::decode(&mut buf).map_err(RaceRecordError::Model)?;
         let actions = Vec::decode(&mut buf).map_err(RaceRecordError::Actions)?;
 
-        Ok(Self { actions })
+        Ok(Self { model, actions })
     }
 }
