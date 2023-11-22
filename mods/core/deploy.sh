03ce6ac23022965c04ea1a8ec1533562d90ab0dd
MrGunflame <mrgunflame@protonmail.com>
2023-11-22 09:29:57 +0100

    Add projectile script

---
+++ mods/core/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ./build/core.mod ../../game_server/mods/core.mod
+cp ./build/core.mod ../../game_client/mods/core.mod
+
+cp ./build/scripts/* ../../game_server/scripts/
+cp ./build/scripts/* ../../game_client/scripts/
+
---
+++ mods/core/scripts/weapon_projectile/src/lib.rs
@@ -0,0 +1,27 @@
+use game_wasm::entity::EntityId;
+use game_wasm::events::on_collision;
+use game_wasm::world::Entity;
+use shared::components::{HEALTH, PROJECTILE_PROPERTIES};
+use shared::{Health, ProjectileProperties};
+
+#[on_collision]
+fn on_collision(entity: EntityId, other: EntityId) {
+    let entity = Entity::get(entity).unwrap();
+
+    let props = entity
+        .components()
+        .get(PROJECTILE_PROPERTIES)
+        .unwrap()
+        .read::<ProjectileProperties>();
+    entity.despawn().unwrap();
+
+    let target = Entity::get(other).unwrap();
+
+    let Ok(mut health) = target.components().get(HEALTH) else {
+        return;
+    };
+
+    health.update::<Health, _, _>(|health| health.0 -= props.damage);
+
+    target.components().insert(HEALTH, &health).unwrap();
+}
