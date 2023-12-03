c985980c7147c052fd3b88550709732d1ebadc5f
MrGunflame <mrgunflame@protonmail.com>
2023-12-03 16:50:12 +0100

    Update core models

--- mods/core/deploy.sh
+++ mods/core/deploy.sh
@@ -6,3 +6,5 @@ cp ./build/core.mod ../../game_client/mods/core.mod
 cp ./build/scripts/* ../../game_server/scripts/
 cp ./build/scripts/* ../../game_client/scripts/
 
+cp ./models/* ../../game_server/assets/
+cp ./models/* ../../game_client/assets/
