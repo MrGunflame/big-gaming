3125a18c994a41a01366b54a08deb9549a802de2
MrGunflame <mrgunflame@protonmail.com>
2024-01-16 23:56:06 +0100

    Fix hard deadlock if the client updates too slow

--- game_client/src/world/game_world.rs
+++ game_client/src/world/game_world.rs
@@ -27,6 +27,11 @@ use crate::world::script::run_scripts;
 
 use super::state::WorldState;
 
+// The maximum number of update cycles allowed per frame. This prevents situations
+// where the update takes longer than the frame and therefore causes the game loop
+// to fall even further behind and never return.
+const MAX_UPDATES_PER_FRAME: u32 = 10;
+
 #[derive(Debug)]
 pub struct GameWorld<I> {
     conn: ServerConnection,
@@ -71,7 +76,13 @@ where
     pub fn update(&mut self, time: &Time, modules: &Modules, cmd_buffer: &mut CommandBuffer) {
         let _span = trace_span!("GameWorld::update").entered();
 
-        while self.game_tick.interval.is_ready(time.last_update()) {
+        // Limit the amount of times we can update to prevent a further falling
+        // behind if the update itself takes longer than the frame.
+        for _ in 0..MAX_UPDATES_PER_FRAME {
+            if !self.game_tick.interval.is_ready(time.last_update()) {
+                break;
+            }
+
             self.conn.update();
 
             self.game_tick.current_control_frame += 1;
