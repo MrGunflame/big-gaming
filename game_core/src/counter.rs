c28fec7c742c2119597de5affd0c46c67e955bc4
MrGunflame <mrgunflame@protonmail.com>
2024-02-08 11:18:18 +0100

    Make `UpdateCounter` track frame times instead of ups

--- game_core/src/counter.rs
+++ game_core/src/counter.rs
@@ -3,10 +3,8 @@ use std::time::{Duration, Instant};
 /// Updates per second.
 #[derive(Clone, Debug)]
 pub struct UpdateCounter {
-    ups: f32,
-    prev: Instant,
-    /// Update period end
-    end: Instant,
+    last_update: Instant,
+    frame_time: Duration,
 }
 
 impl UpdateCounter {
@@ -14,29 +12,21 @@ impl UpdateCounter {
         let now = Instant::now();
 
         Self {
-            ups: 60.0,
-            prev: now,
-            end: now + Duration::from_secs(1),
+            last_update: now,
+            frame_time: Duration::ZERO,
         }
     }
 
     pub fn update(&mut self) {
         let now = Instant::now();
 
-        if now >= self.end {
-            //self.prev = now;
-            self.end = now + Duration::from_secs(1);
-        }
-
-        let frame_time = now - self.prev;
-        self.prev = now;
-        let ups = Duration::from_secs(1).as_secs_f32() / frame_time.as_secs_f32();
-
-        self.ups = (self.ups * 0.8) + (ups * 0.2);
+        let elapsed = now - self.last_update;
+        self.last_update = now;
+        self.frame_time = (self.frame_time.mul_f32(0.8)) + (elapsed.mul_f32(0.2));
     }
 
     pub fn ups(&self) -> f32 {
-        self.ups
+        Duration::from_secs(1).as_secs_f32() / self.frame_time.as_secs_f32()
     }
 }
 
