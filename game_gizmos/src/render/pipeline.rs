7c732211d179666214d0f7985c6a2d189a8fd051
MrGunflame <mrgunflame@protonmail.com>
2024-04-13 00:58:40 +0200

    Render all gizmo lines in a single indirect draw call

--- game_gizmos/src/render/pipeline.rs
+++ game_gizmos/src/render/pipeline.rs
@@ -5,16 +5,15 @@ use game_render::camera::{Camera, CameraUniform};
 use game_render::graph::{Node, RenderContext};
 use game_tracing::trace_span;
 use parking_lot::{Mutex, RwLock};
-use wgpu::hal::auxil::db;
-use wgpu::util::{BufferInitDescriptor, DeviceExt};
+use wgpu::util::{BufferInitDescriptor, DeviceExt, DrawIndirectArgs};
 use wgpu::{
     BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindGroupLayoutDescriptor,
-    BindGroupLayoutEntry, BindingType, BlendState, Buffer, BufferAddress, BufferBindingType,
-    BufferUsages, ColorTargetState, ColorWrites, Device, Face, FragmentState, FrontFace, LoadOp,
+    BindGroupLayoutEntry, BindingType, BlendState, BufferBindingType, BufferUsages,
+    ColorTargetState, ColorWrites, Device, Face, FragmentState, FrontFace, LoadOp,
     MultisampleState, Operations, PipelineLayoutDescriptor, PolygonMode, PrimitiveState,
-    PrimitiveTopology, Queue, RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline,
+    PrimitiveTopology, RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline,
     RenderPipelineDescriptor, ShaderModuleDescriptor, ShaderSource, ShaderStages, StoreOp,
-    TextureFormat, VertexAttribute, VertexBufferLayout, VertexFormat, VertexState, VertexStepMode,
+    TextureFormat, VertexState,
 };
 
 use super::DrawCommand;
@@ -30,16 +29,28 @@ impl GizmoPipeline {
     pub fn new(device: &Device) -> Self {
         let bind_group_layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
             label: Some("gizmo_layout"),
-            entries: &[BindGroupLayoutEntry {
-                binding: 0,
-                visibility: ShaderStages::VERTEX,
-                ty: BindingType::Buffer {
-                    ty: BufferBindingType::Uniform,
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
+            entries: &[
+                BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: ShaderStages::VERTEX,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
                 },
-                count: None,
-            }],
+                BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: ShaderStages::VERTEX,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Storage { read_only: true },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
         });
 
         let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
@@ -59,7 +70,7 @@ impl GizmoPipeline {
             vertex: VertexState {
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[Vertex::layout()],
+                buffers: &[],
             },
             fragment: Some(FragmentState {
                 module: &shader,
@@ -95,7 +106,7 @@ pub struct GizmoPass {
     pipeline: GizmoPipeline,
     camera: Arc<Mutex<Option<Camera>>>,
     elements: Arc<RwLock<Vec<DrawCommand>>>,
-    gpu_elements: Mutex<Vec<GpuElement>>,
+    vertex_buffer: Mutex<Vec<Vertex>>,
 }
 
 impl GizmoPass {
@@ -107,24 +118,27 @@ impl GizmoPass {
         Self {
             pipeline: GizmoPipeline::new(device),
             elements,
-            gpu_elements: Mutex::new(Vec::new()),
             camera,
+            vertex_buffer: Mutex::new(Vec::new()),
         }
     }
 
-    fn update_buffers(&self, device: &Device) {
-        let mut gpu_elements = self.gpu_elements.lock();
-        gpu_elements.clear();
-
+    fn update_buffers(&self) {
         let cmds = self.elements.read();
 
-        let mut elems = Vec::new();
-        for cmd in &*cmds {
-            let elem = GpuElement::new(device, cmd);
-            elems.push(elem);
-        }
+        let mut vertex_buffer = self.vertex_buffer.lock();
+        vertex_buffer.clear();
 
-        *gpu_elements = elems;
+        for cmd in &*cmds {
+            vertex_buffer.push(Vertex {
+                position: cmd.start.extend(0.0).to_array(),
+                color: cmd.color.as_rgba(),
+            });
+            vertex_buffer.push(Vertex {
+                position: cmd.end.extend(0.0).to_array(),
+                color: cmd.color.as_rgba(),
+            });
+        }
     }
 }
 
@@ -136,6 +150,15 @@ impl Node for GizmoPass {
             return;
         };
 
+        self.update_buffers();
+        let vertex_buffer = self.vertex_buffer.lock();
+
+        // Don't start a render pass with 0 vertices, this will cause problems
+        // because the vertex SSBO must contain at least one element.
+        if vertex_buffer.len() == 0 {
+            return;
+        }
+
         let camera_buffer = ctx.device.create_buffer_init(&BufferInitDescriptor {
             label: None,
             contents: bytemuck::cast_slice(&[CameraUniform::new(
@@ -145,17 +168,38 @@ impl Node for GizmoPass {
             usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
         });
 
+        let vertices = ctx.device.create_buffer_init(&BufferInitDescriptor {
+            label: None,
+            contents: bytemuck::cast_slice(&vertex_buffer),
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_DST,
+        });
+
         let bg = ctx.device.create_bind_group(&BindGroupDescriptor {
             label: None,
             layout: &self.pipeline.bind_group_layout,
-            entries: &[BindGroupEntry {
-                binding: 0,
-                resource: camera_buffer.as_entire_binding(),
-            }],
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: camera_buffer.as_entire_binding(),
+                },
+                BindGroupEntry {
+                    binding: 1,
+                    resource: vertices.as_entire_binding(),
+                },
+            ],
         });
 
-        self.update_buffers(ctx.device);
-        let elements = self.gpu_elements.lock();
+        let indirect_buffer = ctx.device.create_buffer_init(&BufferInitDescriptor {
+            label: None,
+            contents: DrawIndirectArgs {
+                vertex_count: 2,
+                instance_count: (vertex_buffer.len() / 2) as u32,
+                first_vertex: 0,
+                first_instance: 0,
+            }
+            .as_bytes(),
+            usage: BufferUsages::INDIRECT,
+        });
 
         let mut render_pass = ctx.encoder.begin_render_pass(&RenderPassDescriptor {
             label: Some("gizmo_pass"),
@@ -174,71 +218,14 @@ impl Node for GizmoPass {
 
         render_pass.set_pipeline(&self.pipeline.pipeline);
 
-        for elem in &*elements {
-            render_pass.set_bind_group(0, &bg, &[]);
-            render_pass.set_vertex_buffer(0, elem.vertices.slice(..));
-
-            render_pass.draw(0..elem.num_vertices, 0..1);
-        }
-    }
-}
-
-struct GpuElement {
-    vertices: Buffer,
-    num_vertices: u32,
-}
-
-impl GpuElement {
-    fn new(device: &Device, cmd: &DrawCommand) -> Self {
-        let vertices = [
-            Vertex {
-                position: cmd.start.to_array(),
-                color: cmd.color.as_rgba(),
-            },
-            Vertex {
-                position: cmd.end.to_array(),
-                color: cmd.color.as_rgba(),
-            },
-        ];
-        let num_vertices = 2;
-
-        let vertices = device.create_buffer_init(&BufferInitDescriptor {
-            label: None,
-            contents: bytemuck::cast_slice(&vertices),
-            usage: BufferUsages::VERTEX,
-        });
-
-        Self {
-            vertices,
-            num_vertices,
-        }
+        render_pass.set_bind_group(0, &bg, &[]);
+        render_pass.draw_indirect(&indirect_buffer, 0);
     }
 }
 
 #[derive(Copy, Clone, Debug, Zeroable, Pod)]
 #[repr(C)]
 struct Vertex {
-    position: [f32; 3],
+    position: [f32; 4],
     color: [f32; 4],
-}
-
-impl Vertex {
-    fn layout<'a>() -> VertexBufferLayout<'a> {
-        VertexBufferLayout {
-            array_stride: std::mem::size_of::<Self>() as BufferAddress,
-            step_mode: VertexStepMode::Vertex,
-            attributes: &[
-                VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: VertexFormat::Float32x3,
-                },
-                VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 3]>() as BufferAddress,
-                    shader_location: 1,
-                    format: VertexFormat::Float32x4,
-                },
-            ],
-        }
-    }
 }
