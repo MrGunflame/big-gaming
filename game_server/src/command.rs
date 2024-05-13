f10760d0b63b2e8c8cb911bb7848d0e71d0916a9
MrGunflame <mrgunflame@protonmail.com>
2024-05-13 15:16:58 +0200

    Add parser for game commands

--- game_server/src/command.rs
+++ game_server/src/command.rs
@@ -1,14 +1,16 @@
-use game_core::command::{ParseError, ServerCommand, Token};
+use game_core::command::{GameCommand, ParseError, ServerCommand, Token};
 
 #[derive(Clone, Debug)]
 pub enum Command {
     Server(ServerCommand),
+    Game(GameCommand),
 }
 
 impl Command {
     pub fn parse(tokens: &[Token<'_>]) -> Result<Self, ParseError> {
         match tokens.first() {
             Some(Token::Ident("server")) => Ok(Self::Server(ServerCommand::parse(&tokens[1..])?)),
+            Some(Token::Ident("game")) => Ok(Self::Game(GameCommand::parse(&tokens[1..])?)),
             _ => Err(ParseError::Empty),
         }
     }
