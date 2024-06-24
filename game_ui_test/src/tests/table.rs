98e9613e827fb2c7592d2e7d7164b1dd6a368e80
MrGunflame <mrgunflame@protonmail.com>
2024-06-25 01:22:56 +0200

    Fix UI table tests

--- game_ui_test/src/tests/table.rs
+++ game_ui_test/src/tests/table.rs
@@ -1,12 +1,12 @@
 use game_ui::reactive::Context;
-use game_ui::widgets::{Table, Widget};
+use game_ui::widgets::{Table, Text, Widget};
 
 pub fn table(ctx: Context<()>) {
     let table = Table {
-        header: vec!["A".to_owned(), "B".to_owned(), "C".to_owned()],
+        header: vec![Text::new("A"), Text::new("B"), Text::new("C")],
         rows: vec![
-            vec!["1".to_owned(), "2".to_owned(), "3".to_owned()],
-            vec!["4".to_owned(), "5".to_owned(), "6".to_owned()],
+            vec![Text::new("1"), Text::new("2"), Text::new("3")],
+            vec![Text::new("4"), Text::new("5"), Text::new("6")],
         ],
     };
     table.mount(&ctx);
