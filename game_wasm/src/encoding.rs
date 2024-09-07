3abe084ae0b89fd6de3856b7c488d66e34f373d6
MrGunflame <mrgunflame@protonmail.com>
2024-09-08 00:24:18 +0200

    Implement `Encode` and `Decode` for `u128` and `i128`

--- game_wasm/src/encoding.rs
+++ game_wasm/src/encoding.rs
@@ -311,7 +311,7 @@ macro_rules! impl_primitive {
     };
 }
 
-impl_primitive! { u16, u32, u64, i8, i16, i32, i64, f32, f64 }
+impl_primitive! { u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64 }
 
 macro_rules! impl_as_array {
     ($($t:ty),*) => {
