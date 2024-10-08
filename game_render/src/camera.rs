1c40e1fa0eda296c450453ca9f205082a47c135e
MrGunflame <mrgunflame@protonmail.com>
2024-10-09 01:43:34 +0200

    Add extra methods to `RenderTarget`

--- game_render/src/camera.rs
+++ game_render/src/camera.rs
@@ -51,6 +51,38 @@ pub enum RenderTarget {
     Image(RenderImageId),
 }
 
+impl RenderTarget {
+    /// Returns `true` if this `RenderTarget` is a `Window`.
+    #[inline]
+    pub const fn is_window(&self) -> bool {
+        matches!(self, Self::Window(_))
+    }
+
+    /// Returns `true` if this `RenderTarget` is a `Image`.
+    #[inline]
+    pub const fn is_image(&self) -> bool {
+        matches!(self, Self::Image(_))
+    }
+
+    /// Returns the underlying [`WindowId`] or `None` if this `RenderTarget` is not `Window`.
+    #[inline]
+    pub const fn as_window(&self) -> Option<&WindowId> {
+        match self {
+            Self::Window(window) => Some(window),
+            Self::Image(_) => None,
+        }
+    }
+
+    /// Returns the underlying [`RenderImageId`] or `None` if this `RenderTarget` is not `Image`.
+    #[inline]
+    pub const fn as_image(&self) -> Option<&RenderImageId> {
+        match self {
+            Self::Image(image) => Some(image),
+            Self::Window(_) => None,
+        }
+    }
+}
+
 impl From<WindowId> for RenderTarget {
     #[inline]
     fn from(value: WindowId) -> Self {
