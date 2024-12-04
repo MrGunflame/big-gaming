43af6b32ab9e20fc523ad871609e4e83f59d342d
MrGunflame <mrgunflame@protonmail.com>
2024-12-04 16:14:02 +0100

    Try all permutations when computing render graph schedule

--- game_render/src/graph/scheduler.rs
+++ game_render/src/graph/scheduler.rs
@@ -1,32 +1,19 @@
 use std::collections::HashMap;
 
-use super::{Dependency, NodeLabel, RenderGraph, SlotFlags};
+use thiserror::Error;
+
+use super::{Dependency, NodeLabel, RenderGraph, SlotFlags, SlotLabel};
 
 pub struct RenderGraphScheduler;
 
 impl RenderGraphScheduler {
     pub fn schedule(&mut self, graph: &RenderGraph) -> Result<Vec<NodeLabel>, ScheduleError> {
-        let mut write_slots: HashMap<&super::SlotLabel, Vec<_>> = HashMap::new();
-        let mut read_slots: HashMap<&super::SlotLabel, Vec<_>> = HashMap::new();
+        let mut write_slots: HashMap<&SlotLabel, Vec<_>> = HashMap::new();
+        let mut read_slots: HashMap<&SlotLabel, Vec<_>> = HashMap::new();
 
-        let mut dependency_list = HashMap::new();
-
-        for node in graph.nodes.values() {
-            for dep in &node.dependencies {
-                match dep {
-                    Dependency::Node(_) => (),
-                    Dependency::Slot(label, kind, flags) => {
-                        if flags.contains(SlotFlags::READ) {
-                            read_slots.entry(label).or_default().push(node.label);
-                        }
-
-                        if flags.contains(SlotFlags::WRITE) {
-                            write_slots.entry(label).or_default().push(node.label);
-                        }
-                    }
-                }
-            }
-        }
+        // Dependencies that must always be satisfied
+        // regardless of permutation.
+        let mut fixed_dependencies = HashMap::new();
 
         for node in graph.nodes.values() {
             let mut node_deps = Vec::new();
@@ -38,53 +25,209 @@ impl RenderGraphScheduler {
                     }
                     Dependency::Slot(label, kind, flags) => {
                         if flags.contains(SlotFlags::READ) {
-                            let src = write_slots.get(&label).unwrap();
-                            assert!(!src.is_empty());
+                            read_slots.entry(label).or_default().push(node.label);
+                        }
 
-                            node_deps.extend(src);
+                        if flags.contains(SlotFlags::WRITE) {
+                            write_slots.entry(label).or_default().push(node.label);
                         }
                     }
                 }
             }
 
-            dependency_list.insert(node.label, node_deps);
+            fixed_dependencies.insert(node.label, node_deps);
         }
 
-        let mut output = Vec::new();
+        let mut dependency_options: HashMap<NodeLabel, HashMap<&SlotLabel, Vec<&NodeLabel>>> =
+            HashMap::new();
 
-        loop {
-            if dependency_list.is_empty() {
-                return Ok(output);
-            }
+        for node in graph.nodes.values() {
+            for dep in &node.dependencies {
+                match dep {
+                    Dependency::Node(_) => {}
+                    Dependency::Slot(label, kind, flags) => {
+                        // Requires that another node that has written to
+                        // this slot is ran before.
+                        if flags.contains(SlotFlags::READ) {
+                            let src = write_slots
+                                .get(&label)
+                                .unwrap()
+                                .iter()
+                                // If the node reads and writes to a slot it
+                                // must not be scheduled as its own predecessor.
+                                .filter(|src| **src != node.label)
+                                // If the node that provides the dependency is always
+                                // scheduled after the current node it cannot possibly
+                                // provide the slot for this node.
+                                .filter(|src| match fixed_dependencies.get(&src) {
+                                    Some(nodes) => !nodes.contains(&&node.label),
+                                    None => true,
+                                })
+                                .collect::<Vec<_>>();
 
-            let mut remove_node = None;
+                            // Cannot schedule the node if no nodes providing the
+                            // required dependencies exist.
+                            if src.is_empty() {
+                                return Err(ScheduleError::NoSource(node.label, *label));
+                            }
 
-            for (node, dependencies) in dependency_list.iter() {
-                if dependencies.is_empty() {
-                    remove_node = Some(*node);
-                    break;
-                }
-            }
+                            // If only a single possible node exists we already
+                            // have found the only possible option.
+                            if src.len() == 1 {
+                                // If the node reads and writes to a slot it
+                                // must not be scheduled as its own predecessor.
+                                debug_assert_ne!(&node.label, src[0]);
 
-            match remove_node {
-                Some(node) => {
-                    dependency_list.remove(&node);
-                    for deps in dependency_list.values_mut() {
-                        deps.retain(|dep| **dep != node);
+                                fixed_dependencies
+                                    .entry(node.label)
+                                    .or_default()
+                                    .push(&src[0]);
+                                continue;
+                            }
+
+                            dependency_options
+                                .entry(node.label)
+                                .or_default()
+                                .insert(label, src);
+                        }
                     }
-                    output.push(node);
-                }
-                None => {
-                    return Err(ScheduleError::Cycle);
                 }
             }
+        }
+
+        // Invert the `dependency_options` strucuture and create a
+        // set of permutations to check.
+        let mut permutations = PermutationSet::new();
+        for (node, slots) in &dependency_options {
+            for opts in slots.values() {
+                let mut values = Vec::new();
+
+                for opt in opts {
+                    values.push(DependencyOption {
+                        node,
+                        depends_on: opt,
+                    });
+                }
+
+                permutations.add(&values);
+            }
+        }
+
+        let mut permutations_iter = permutations.iter();
+
+        let mut output = Vec::new();
+        loop {
+            output.clear();
+
+            let mut dependency_list = fixed_dependencies.clone();
+            match permutations_iter.next() {
+                Some(deps) => {
+                    for dep in deps {
+                        let node_deps = dependency_list.entry(*dep.node).or_default();
+
+                        // The dependency list should never contain a node more than once.
+                        // If it already exists the dependency is already satisfied and
+                        // we don't have to add it.
+                        if !node_deps.contains(&dep.node) {
+                            node_deps.push(dep.depends_on);
+                        }
+                    }
+                }
+                // If we have no permutations ignore the permutations iterator
+                // entirely. We always have a fixed pipeline.
+                None if permutations.is_empty() => (),
+                // If we reach this point we have tried all possible
+                // permutations and none of them are valid.
+                None => return Err(ScheduleError::NoValidPermutation),
+            }
+
+            while !dependency_list.is_empty() {
+                let mut remove_node = None;
+                for (node, dependencies) in dependency_list.iter() {
+                    if dependencies.is_empty() {
+                        remove_node = Some(*node);
+                        break;
+                    }
+                }
+
+                match remove_node {
+                    Some(node) => {
+                        dependency_list.remove(&node);
+                        for dependencies in dependency_list.values_mut() {
+                            dependencies.retain(|dep| **dep != node);
+                        }
+
+                        output.push(node);
+                    }
+                    // No node could be scheduled this iteration.
+                    // This means we have a cycle.
+                    None => return Err(ScheduleError::Cycle),
+                }
+            }
+
+            return Ok(output);
         }
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Error)]
 pub enum ScheduleError {
+    #[error("cycle detected")]
     Cycle,
+    #[error("no source for slot {1:?} for node {0:?}")]
+    NoSource(NodeLabel, SlotLabel),
+    #[error("no valid permutation for slot configuration")]
+    NoValidPermutation,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct DependencyOption<'a> {
+    node: &'a NodeLabel,
+    depends_on: &'a NodeLabel,
+}
+
+/// A set that mixes sets of possible permutations together.
+#[derive(Clone, Debug)]
+struct PermutationSet<T> {
+    sets: Vec<Vec<T>>,
+}
+
+impl<T> PermutationSet<T>
+where
+    T: Copy,
+{
+    fn new() -> Self {
+        Self { sets: Vec::new() }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.sets.is_empty()
+    }
+
+    /// Adds a new set of possible values to the set.
+    fn add(&mut self, values: &[T]) {
+        if self.sets.is_empty() {
+            for value in values {
+                self.sets.push(vec![*value]);
+            }
+        } else {
+            let mut new_sets = Vec::new();
+            for value in values {
+                for set in &self.sets {
+                    let mut new_set = set.clone();
+                    new_set.push(*value);
+                    new_sets.push(new_set);
+                }
+            }
+
+            self.sets = new_sets;
+        }
+    }
+
+    /// Returns an iterator over all possible permutations in this set.
+    fn iter(&self) -> impl Iterator<Item = &[T]> {
+        self.sets.iter().map(|v| v.as_slice())
+    }
 }
 
 #[cfg(test)]
@@ -93,7 +236,7 @@ mod test {
         Node, NodeLabel, RenderContext, RenderGraph, SlotFlags, SlotKind, SlotLabel,
     };
 
-    use super::{RenderGraphScheduler, ScheduleError};
+    use super::{PermutationSet, RenderGraphScheduler, ScheduleError};
 
     struct TestNode;
 
@@ -187,5 +330,104 @@ mod test {
 
         let res = RenderGraphScheduler.schedule(&graph);
         assert_eq!(res, Err(ScheduleError::Cycle));
+    }
+
+    #[test]
+    fn render_graph_read_write_slot() {
+        let a = NodeLabel::new("A");
+        let b = NodeLabel::new("B");
+
+        let mut graph = RenderGraph::new();
+        graph.add_node(a, TestNode);
+        graph.add_node(b, TestNode);
+
+        graph.add_slot_dependency(
+            a,
+            SlotLabel::new("test"),
+            SlotKind::Texture,
+            SlotFlags::WRITE,
+        );
+
+        graph.add_slot_dependency(
+            b,
+            SlotLabel::new("test"),
+            SlotKind::Buffer,
+            SlotFlags::READ | SlotFlags::WRITE,
+        );
+
+        let res = RenderGraphScheduler.schedule(&graph).unwrap();
+        assert_eq!(res, [a, b]);
+    }
+
+    #[test]
+    fn render_graph_many_permutations() {
+        let a = NodeLabel::new("A");
+        let b = NodeLabel::new("B");
+        let c = NodeLabel::new("C");
+        let d = NodeLabel::new("D");
+        let e = NodeLabel::new("E");
+
+        let x = SlotLabel::new("x");
+        let y = SlotLabel::new("y");
+
+        let mut graph = RenderGraph::new();
+        graph.add_node(a, TestNode);
+        graph.add_node(b, TestNode);
+        graph.add_node(c, TestNode);
+        graph.add_node(d, TestNode);
+        graph.add_node(e, TestNode);
+
+        // A | B => C
+        // D | E => C
+        graph.add_slot_dependency(a, x, SlotKind::Buffer, SlotFlags::WRITE);
+        graph.add_slot_dependency(b, x, SlotKind::Buffer, SlotFlags::WRITE);
+        graph.add_slot_dependency(c, x, SlotKind::Buffer, SlotFlags::READ);
+        graph.add_slot_dependency(c, y, SlotKind::Buffer, SlotFlags::READ);
+        graph.add_slot_dependency(d, y, SlotKind::Buffer, SlotFlags::WRITE);
+        graph.add_slot_dependency(e, y, SlotKind::Buffer, SlotFlags::WRITE);
+
+        let res = RenderGraphScheduler.schedule(&graph).unwrap();
+        let index_a = res.iter().position(|v| *v == a).unwrap();
+        let index_b = res.iter().position(|v| *v == b).unwrap();
+        let index_c = res.iter().position(|v| *v == c).unwrap();
+        let index_d = res.iter().position(|v| *v == d).unwrap();
+        let index_e = res.iter().position(|v| *v == e).unwrap();
+
+        // A or B before C
+        // D or E before C
+        assert!(index_a < index_c || index_b < index_c);
+        assert!(index_d < index_c || index_e < index_c);
+    }
+
+    #[test]
+    fn permutation_set() {
+        let mut set = PermutationSet::new();
+        set.add(&["A", "B"]);
+        assert_eq!(set.iter().collect::<Vec<_>>(), [["A"], ["B"]]);
+
+        set.add(&["X", "Y"]);
+        assert_eq!(
+            set.iter().collect::<Vec<_>>(),
+            [["A", "X"], ["B", "X"], ["A", "Y"], ["B", "Y"]]
+        );
+
+        set.add(&["E", "F", "G"]);
+        assert_eq!(
+            set.iter().collect::<Vec<_>>(),
+            [
+                ["A", "X", "E"],
+                ["B", "X", "E"],
+                ["A", "Y", "E"],
+                ["B", "Y", "E"],
+                ["A", "X", "F"],
+                ["B", "X", "F"],
+                ["A", "Y", "F"],
+                ["B", "Y", "F"],
+                ["A", "X", "G"],
+                ["B", "X", "G"],
+                ["A", "Y", "G"],
+                ["B", "Y", "G"],
+            ]
+        );
     }
 }
