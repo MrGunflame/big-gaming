90f1726eba09c9ae29c6a1e7a4e2f259125a50e7
MrGunflame <mrgunflame@protonmail.com>
2025-05-27 18:30:14 +0200

    Add extra `VecMap` methods/implementations

--- game_common/src/collections/vec_map.rs
+++ game_common/src/collections/vec_map.rs
@@ -16,6 +16,12 @@ impl Index for usize {
     }
 }
 
+impl Index for u32 {
+    fn index(&self) -> usize {
+        *self as usize
+    }
+}
+
 /// A key-value map backed by a [`Vec`].
 ///
 /// This is more efficient alternative to a [`HashMap`] when the keys follow a linear pattern.
@@ -106,6 +112,10 @@ where
         }
     }
 
+    pub fn values(&self) -> Values<'_, K, V> {
+        Values { iter: self.iter() }
+    }
+
     pub fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
         ValuesMut {
             iter: self.iter_mut(),
@@ -116,11 +126,53 @@ where
         self.inner.clear();
         self.len = 0;
     }
+
+    pub fn into_values(self) -> IntoValues<K, V> {
+        IntoValues {
+            iter: self.into_iter(),
+        }
+    }
 }
 
 impl<K, V> Default for VecMap<K, V> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl<'a, K, V> IntoIterator for &'a VecMap<K, V>
+where
+    K: Index,
+{
+    type Item = (K, &'a V);
+    type IntoIter = Iter<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, K, V> IntoIterator for &'a mut VecMap<K, V>
+where
+    K: Index,
+{
+    type Item = (K, &'a mut V);
+    type IntoIter = IterMut<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
+    }
+}
+
+impl<K, V> IntoIterator for VecMap<K, V> {
+    type Item = (K, V);
+    type IntoIter = IntoIter<K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter {
+            iter: self.inner.into_iter(),
+            len: self.len,
+        }
     }
 }
 
@@ -130,6 +182,7 @@ enum Entry<K, V> {
     Occupied((K, V)),
 }
 
+#[derive(Clone, Debug)]
 pub struct Iter<'a, K, V> {
     iter: std::slice::Iter<'a, Entry<K, V>>,
     len: usize,
@@ -209,6 +262,75 @@ where
 
 impl<'a, K, V> FusedIterator for IterMut<'a, K, V> where K: Index {}
 
+#[derive(Clone, Debug)]
+pub struct IntoIter<K, V> {
+    iter: std::vec::IntoIter<Entry<K, V>>,
+    len: usize,
+}
+
+impl<K, V> Iterator for IntoIter<K, V> {
+    type Item = (K, V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let entry = self.iter.next()?;
+            match entry {
+                Entry::None => (),
+                Entry::Occupied((key, val)) => {
+                    self.len -= 1;
+                    return Some((key, val));
+                }
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<K, V> ExactSizeIterator for IntoIter<K, V> {
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<K, V> FusedIterator for IntoIter<K, V> {}
+
+#[derive(Clone, Debug)]
+pub struct Values<'a, K, V>
+where
+    K: Index,
+{
+    iter: Iter<'a, K, V>,
+}
+
+impl<'a, K, V> Iterator for Values<'a, K, V>
+where
+    K: Index,
+{
+    type Item = &'a V;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|(_, v)| v)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<'a, K, V> ExactSizeIterator for Values<'a, K, V>
+where
+    K: Index,
+{
+    fn len(&self) -> usize {
+        self.iter.len()
+    }
+}
+
+impl<'a, K, V> FusedIterator for Values<'a, K, V> where K: Index {}
+
 #[derive(Debug)]
 pub struct ValuesMut<'a, K, V>
 where
@@ -242,3 +364,44 @@ where
 }
 
 impl<'a, K, V> FusedIterator for ValuesMut<'a, K, V> where K: Index {}
+
+#[derive(Clone, Debug)]
+pub struct IntoValues<K, V> {
+    iter: IntoIter<K, V>,
+}
+
+impl<K, V> Iterator for IntoValues<K, V> {
+    type Item = V;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|(_, value)| value)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<K, V> ExactSizeIterator for IntoValues<K, V> {
+    fn len(&self) -> usize {
+        self.iter.len()
+    }
+}
+
+impl<K, V> FusedIterator for IntoValues<K, V> {}
+
+impl<K, V> FromIterator<(K, V)> for VecMap<K, V>
+where
+    K: Index,
+{
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = (K, V)>,
+    {
+        let mut map = Self::new();
+        for (key, value) in iter {
+            map.insert(key, value);
+        }
+        map
+    }
+}
